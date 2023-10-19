d899cdc7d848ec4843bd14734ff4f96bb1a045cb
KingsleyAzuka <kingsleyazuks@gmail.com>
2023-10-19 05:16:57 -0700

    eight file

--- AIapp/urls.py
+++ AIapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [ 
        path('', views.index), 
-    path('menu/', views.home, name='menu'), 
+    path('home/', views.home, name='home'), 
     path('register/', views.register, name='register'),  
     path('login/', views.login, name='login')
 ] 
